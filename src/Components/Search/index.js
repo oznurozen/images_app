c35e71a9ab5c3b9db1eefaa2a58c70b3498fff73
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2022-10-23 14:27:19 +0300

    firstcommit

---
+++ src/Components/Search/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const Search = () => {
+  return (
+    <div className="search-bar-container ui input">
+        <input type="text" placeholder="Ara..."/>
+    </div>
+    
+  )
+}
+
+export default Search
