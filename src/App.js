202bcb2d4518fa4bbfdf8b9cc28ed6f62ed2924d
Süleyman ÖZDAMAR <101826875+SuleymanOzdamar@users.noreply.github.com>
2022-10-23 15:03:33 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import Search from './Components/Search'
+import React, { Component } from 'react'
+import Search from './Components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <Search/>
-    </div>
-  );
+class App extends Component {
+  onSearchImage = search => {
+    console.log('App: ' + search )
+  }
+  render() {
+    return (
+      <div className='app-container'>
+        <Search onSearchImage={this.onSearchImage} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,13 +1,39 @@
-import React from 'react'
 import './style.css'
+import React, { Component } from 'react'
 
-const Search = () => {
-  return (
-    <div className="search-bar-container ui input">
-        <input type="text" placeholder="Ara..."/>
-    </div>
+class Search extends Component {
+  state = {
+    search: ''
+  };
+
+  inputChanged = event => {
+    console.log(event.target.value);
+    this.setState({
+      search:event.target.value
+    });
+  }
+
+  searchImage = () => {
+    console.log('başarılı');
+    this.props.onSearchImage(this.state.search)
+  }
+
+  render() {
+    return (
+      <div className="search-bar-container ui input">
+        <input type="text" onKeyPress={(e) => {
+          if(e.key === 'Enter'){
+            this.searchImage();
+          }
+        }} onChange={this.inputChanged.bind(this)} placeholder="Ara..."/>
+        <button className='ui icon button' onClick={this.searchImage}>
+          <i className='search icon'></i>
+        </button>
+      </div>
     
-  )
+    )
+  }
 }
 
 export default Search
+
